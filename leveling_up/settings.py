c8c0a194daa2fefe099fbad0da5cc7047bfaffa6
himanshi <hv.himanshi.verma@gmail.com>
2020-04-10 15:08:29 +0530

    show score in the format of round and commas

--- leveling_up/settings.py
+++ leveling_up/settings.py
@@ -18,10 +18,11 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.fleet_drop_speed = 30
+        self.fleet_drop_speed = 25
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -40,3 +41,5 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+    
